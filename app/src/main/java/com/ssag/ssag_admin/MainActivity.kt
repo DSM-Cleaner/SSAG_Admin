b0ae8983dbae00694be01a7e733329ce9cb7e967
Jaewon <turitus@naver.com>
2022-04-08 11:57:30 +0900

    ⚡️:: initial project

---
+++ app/src/main/java/com/ssag/ssag_admin/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.ssag.ssag_admin
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.ssag.ssag_admin.ui.theme.SSAG_AdminTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            SSAG_AdminTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    SSAG_AdminTheme {
+        Greeting("Android")
+    }
+}
