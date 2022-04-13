138b2a2cbe54dd2e370ddef77a54a2bd5b3a3af2
Jaewon <turitus@naver.com>
2022-04-13 15:09:15 +0900

    ⚡️:: room state entity

---
+++ domain/src/main/java/com/ssag/domain/clean/entity/RoomStateEntity.kt
@@ -0,0 +1,17 @@
+package com.ssag.domain.clean.entity
+
+data class RoomStateEntity(
+    val lightIsComplete: Boolean,
+    val plugIsComplete: Boolean,
+    val shoesAreComplete: Boolean,
+    val studentA: StudentEntity,
+    val studentB: StudentEntity,
+    val studentC: StudentEntity
+) {
+    
+    data class StudentEntity(
+        val gcn: Int,
+        val name: String,
+        val cleanState: CleanStateEntity
+    )
+}
