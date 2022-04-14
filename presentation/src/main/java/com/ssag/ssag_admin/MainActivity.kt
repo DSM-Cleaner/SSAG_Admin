e47104f5050edd6af080e0ce2b0e677b0bd3881c
Jaewon <turitus@naver.com>
2022-04-14 19:01:08 +0900

    ⚡️:: app composable func

--- presentation/src/main/java/com/ssag/ssag_admin/MainActivity.kt
+++ presentation/src/main/java/com/ssag/ssag_admin/MainActivity.kt
@@ -6,38 +6,23 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.ssag.ssag_admin.ui.theme.SSAG_AdminTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             SSAG_AdminTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    SsagApp()
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    SSAG_AdminTheme {
-        Greeting("Android")
     }
 }
---
+++ presentation/src/main/java/com/ssag/ssag_admin/SsagApp.kt
@@ -0,0 +1,8 @@
+package com.ssag.ssag_admin
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun SsagApp() {
+
+}
