8b596ef3a863fcdb7eedfdaf5f9e899e683cc3b7
Jaewon <turitus@naver.com>
2022-04-15 19:46:30 +0900

    ⚡️:: login intent viewmodel

--- presentation/src/main/java/com/ssag/ssag_admin/feature/login/LoginViewModel.kt
+++ presentation/src/main/java/com/ssag/ssag_admin/feature/login/LoginViewModel.kt
@@ -1,11 +1,40 @@
 package com.ssag.ssag_admin.feature.login
 
 import androidx.lifecycle.ViewModel
+import com.ssag.domain.auth.usecase.LoginUseCase
+import com.ssag.ssag_admin.base.BaseViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
 class LoginViewModel @Inject constructor(
+    private val loginUseCase: LoginUseCase
+) : BaseViewModel<LoginState, LoginIntent>() {
 
-) : ViewModel() {
+    override val initialState: LoginState
+        get() = LoginState.initial()
+
+    override fun reduceIntent(oldState: LoginState, intent: LoginIntent) {
+        when (intent) {
+            is LoginIntent.SetTeacher -> {
+                setState(
+                    oldState.copy(
+                        hasLogin = true,
+                        teacherName = intent.teacherEntity.name,
+                        startFloor = if (intent.teacherEntity.isManTeacher) 5 else 2
+                    )
+                )
+            }
+
+            is LoginIntent.Logout -> {
+                setState(
+                    oldState.copy(
+                        hasLogin = false,
+                        teacherName = "",
+                        startFloor = 0
+                    )
+                )
+            }
+        }
+    }
 }
