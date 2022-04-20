2c8c4071662a07be38209a560ade42df498371a0
Jaewon <turitus@naver.com>
2022-04-20 15:56:23 +0900

    ⚡️:: color

--- presentation/src/main/java/com/ssag/ssag_admin/ui/theme/Color.kt
+++ presentation/src/main/java/com/ssag/ssag_admin/ui/theme/Color.kt
@@ -5,7 +5,7 @@ import androidx.compose.ui.graphics.Color
 val Purple200 = Color(0xFFE2E1FF)
 val Purple500 = Color(0xFFC4C3FB)
 val Purple700 = Color(0xFFA09EFF)
-val Teal200 = Color(0xFF03DAC5)
+
 val Blue200 = Color(0xFFC0E1FF)
 val Blue500 = Color(0xFF94CBFF)
 val Blue700 = Color(0xFF84B5FF)
