1ef3086a1ce79cea40c22ac72b670035e139221e
Jaewon <turitus@naver.com>
2022-04-24 10:15:00 +0900

    ⚡️:: change password

---
+++ presentation/src/main/java/com/ssag/ssag_admin/feature/changepassword/ChangePassword.kt
@@ -0,0 +1,9 @@
+package com.ssag.ssag_admin.feature.changepassword
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun ChangePassword(navController: NavController){
+    
+}
