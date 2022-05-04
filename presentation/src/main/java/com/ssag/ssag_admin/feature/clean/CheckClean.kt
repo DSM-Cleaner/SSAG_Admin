13f0c355a3ba5175bb7759778ee589d41f5adcf3
Jaewon <turitus@naver.com>
2022-05-04 09:53:56 +0900

    ⚡️:: check clean student

--- presentation/src/main/java/com/ssag/ssag_admin/feature/clean/CheckClean.kt
+++ presentation/src/main/java/com/ssag/ssag_admin/feature/clean/CheckClean.kt
@@ -1,20 +1,27 @@
 package com.ssag.ssag_admin.feature.clean
 
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import com.ssag.domain.clean.entity.StudentEntity
+import com.ssag.ssag_admin.ui.theme.Gray200
 
 @Composable
 fun CheckClean(
@@ -31,7 +38,7 @@ fun CheckClean(
             CheckCleanTopBarContent(
                 checkCleanState = checkCleanState,
                 doOnSelectRoomClick = { },
-                doOnCompleteClick = { }
+                doOnCompleteClick = { navController.popBackStack() }
             )
         }
     ) {
@@ -50,7 +57,6 @@ fun CheckCleanTopBarContent(
             Text(
                 text = "${checkCleanState.roomNumber}호",
                 style = TextStyle(
-                    textDecoration = TextDecoration.Underline,
                     fontWeight = FontWeight.Bold,
                     fontSize = 18.sp
                 ),
@@ -62,6 +68,13 @@ fun CheckCleanTopBarContent(
             )
         },
         actions = {
+            Text(
+                text = "호실선택",
+                fontWeight = FontWeight.SemiBold,
+                modifier = Modifier
+                    .clickable { doOnSelectRoomClick() }
+                    .padding(10.dp)
+            )
             Text(
                 text = "검사완료",
                 fontWeight = FontWeight.SemiBold,
@@ -80,7 +93,61 @@ fun CheckCleanTopBarContent(
 fun CheckCleanContent(
     checkCleanState: CheckCleanState
 ) {
+    val scrollState = rememberScrollState()
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .verticalScroll(scrollState)
+            .padding(12.dp)
+    ) {
+        Text(
+            text = "통과되지 않은 항목을 체크해 주세요.",
+            textAlign = TextAlign.End,
+            fontSize = 13.sp,
+            modifier = Modifier
+                .fillMaxWidth()
+        )
+    }
+}
 
+@Composable
+fun CheckCleanCardView(modifier: Modifier = Modifier, content: @Composable () -> Unit) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(12.dp, 0.dp)
+            .border(1.dp, Gray200, RoundedCornerShape(5.dp))
+    ) {
+        content()
+    }
+}
+
+@Composable
+fun CheckCleanPersonalRow(student: StudentEntity) {
+    Row(modifier = Modifier.fillMaxWidth()) {
+        PersonalInfoView(student = student)
+        Row(horizontalArrangement = Arrangement.SpaceEvenly, modifier = Modifier.fillMaxWidth()) {
+
+        }
+    }
+}
+
+@Composable
+fun PersonalInfoView(student: StudentEntity) {
+    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+        val bedPosition = student.bedPosition
+        val gcn = student.gcn.toString()
+        val name = student.name
+        Text(
+            text = bedPosition,
+            color = Color.Black,
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold
+        )
+        Text(text = gcn)
+        Text(text = name)
+    }
 }
 
 @Preview
@@ -91,4 +158,19 @@ fun CheckCleanTopBarPreview() {
         doOnSelectRoomClick = { },
         doOnCompleteClick = { }
     )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun CheckCleanCardViewPreview() {
+    CheckCleanCardView {
+        Text(text = "test")
+        Text(text = "cardView")
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun CheckCleanContentPreview() {
+    CheckCleanContent(checkCleanState = CheckCleanState.initial())
 }
